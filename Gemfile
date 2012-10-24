33d88b9645a109d700137bdd6baffc5197334a07
Jeffrey Wescott <jeffrey@change.org>
2012-10-24 14:26:39 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+gem 'rake'
+gem 'rspec'
+gem 'activerecord', :require => 'active_record'
+gem 'sqlite3'
---
+++ lib/sunlight_legislators_importer.rb
@@ -0,0 +1,21 @@
+require 'csv'
+
+class SunlightLegislatorsImporter
+  def self.import(filename)
+    csv = CSV.new(File.open(filename), :headers => true)
+    csv.each do |row|
+      row.each do |field, value|
+        raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+      end
+    end
+  end
+end
+
+begin
+  raise ArgumentError, "you must supply a filename argument" unless ARGV.length == 1
+  SunlightLegislatorsImporter.import(ARGV[0])
+rescue ArgumentError => e
+  $stderr.puts "Usage: ruby sunlight_legislators_importer.rb <filename>"
+rescue NotImplementedError => e
+  $stderr.puts "You shouldn't be running this until you've modified it with your implementation!"
+end
