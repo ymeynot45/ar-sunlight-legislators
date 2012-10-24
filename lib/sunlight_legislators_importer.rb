e7c6c29384d69d43b4b3de5a12f875c291e3f0fd
Jeffrey Wescott <jeffrey@change.org>
2012-10-24 14:48:19 -0700

    finish the app layout; add comments to the importer as a hint to the students

--- lib/sunlight_legislators_importer.rb
+++ lib/sunlight_legislators_importer.rb
@@ -5,7 +5,9 @@ class SunlightLegislatorsImporter
     csv = CSV.new(File.open(filename), :headers => true)
     csv.each do |row|
       row.each do |field, value|
+        # TODO: begin
         raise NotImplementedError, "TODO: figure out what to do with this row and do it!"
+        # TODO: end
       end
     end
   end
