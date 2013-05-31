415972e150beb10a4c6155d6403979d4dad24125
William Karsten <ymeynot45@YmeYnot45s-MacBook-Air.local>
2013-05-30 19:35:53 -0500

    time with peter

---
+++ app/models/legislators.rb
@@ -0,0 +1,4 @@
+require_relative '../../db/config'
+
+class Legislators < ActiveRecord::Base
+end
---
+++ db/migrate/20130530163300_create_legislators.rb
@@ -0,0 +1,33 @@
+require_relative '../config'
+
+class CreateLegislators < ActiveRecord::Migration
+  def change
+    create_table :legislators do |t|
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :fax
+      t.string :website
+      t.string :party
+      t.string :gender
+      t.date   :birthdate
+      t.string :twitter_id
+    end
+  end
+end
+
+
+
+
+      # t.string :first_name
+      # t.string :middle_name
+      # t.string :last_name
+      # t.string :name_suffix
+      # t.string :nickname
+      # t.string :party
+      # t.string :state
+      # t.string :district
+      # t.date   :birthday
+      # t.string :gender
+      # t.string :email
+      # t.string :phone
